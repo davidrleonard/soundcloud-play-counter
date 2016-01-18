641f028dd223f71739844b4b75737c3109b29c78
davidrleonard <dleonard@codeforamerica.org>
2016-01-18 15:45:02 -0800

    Add a new method to show discrete play count over the last month.

--- Rakefile
+++ Rakefile
@@ -19,12 +19,15 @@ end
 namespace :one_time_seed do
   task :seed_2015_plays do
     # This is meant to be used once to seed the database. It puts the play
-    # count for Dec. 15 2015 into the database so we can run the right
-    # operations starting from day 1.
+    # count for Dec. 15 2015 and Nov. 18, 2015 into the database so we can
+    # run the right operations starting from day 1.
     require './app'
-    play = Play.create( play_count: 1684397,
-                        created_at: 31.days.ago,
-                        updated_at: 31.days.ago )
+    first_play = Play.create( play_count: 1684397,
+                              created_at: 31.days.ago,
+                              updated_at: 31.days.ago )
+    second_play = Play.create( play_count: 1584497,
+                               created_at: 61.days.ago,
+                               updated_at: 61.days.ago )
     puts 'Seeded play count!'
   end
 end
--- app.rb
+++ app.rb
@@ -13,21 +13,30 @@ get '/' do
 end
 
 get '/api/1/plays.json' do
+  # Get all of our plays
+  plays = Play.all
+
   # Get play count for right now
-  current_plays = Play.order(:created_at).last(1)
+  current_plays = plays.order(:created_at).last(1)
   current_count = current_plays[0].play_count
 
   # Get play count for 30 days ago
-  thirty_days_ago_plays = Play.where(['created_at < ?',30.days.ago]).order(:created_at).last(1)
+  thirty_days_ago_plays = plays.where(['created_at < ?',30.days.ago]).order(:created_at).last(1)
   thirty_days_ago_count = thirty_days_ago_plays[0].play_count
+
+  # Get play count for 60 days ago
+  sixty_days_ago_plays = plays.where(['created_at < ?',60.days.ago]).order(:created_at).last(1)
+  sixty_days_ago_count = sixty_days_ago_plays[0].play_count
 
   json(
         :all_time_plays => {
            :today => current_count,
-           :thirty_days_ago => thirty_days_ago_count
+           :thirty_days_ago => thirty_days_ago_count,
+           :sixty_days_ago => sixty_days_ago_count
          },
          :play_count => {
-           :this_month => current_count - thirty_days_ago_count
+           :this_month => current_count - thirty_days_ago_count,
+           :last_month => thirty_days_ago_count - sixty_days_ago_count
          }
       )
 end
